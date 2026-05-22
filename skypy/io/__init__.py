1bc0c8f32a8d70a85c644015a7c3378bf2594ffc
LediaD <lediadakaj@gmail.com>
2026-05-22 11:51:32 +0200

    chore: add package __init__.py for skypy, models, and io

---
+++ skypy/io/__init__.py
@@ -0,0 +1,13 @@
+from skypy.io.loader import (
+    load_flights_csv,
+    load_crew_csv,
+    parse_flight_dict,
+    parse_crew_dict,
+)
+
+__all__ = [
+    "load_flights_csv",
+    "load_crew_csv",
+    "parse_flight_dict",
+    "parse_crew_dict",
+]
---
+++ skypy/models/__init__.py
@@ -0,0 +1,5 @@
+from skypy.models.flight import Flight
+from skypy.models.crew import Crew
+from skypy.models.roster import Roster
+
+__all__ = ["Flight", "Crew", "Roster"]
